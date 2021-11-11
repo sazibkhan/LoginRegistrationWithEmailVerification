dfe9890873c76ef1c7e6874e35b75dc0dff4b116
sazibkhan <sazibkhancse@gmail.com>
2021-11-11 06:55:58 +0000

    User Login Registration With Email Verification

---
+++ src/main/java/com/asl/LoginRegistrationWithEmailVerificationApplication.java
@@ -0,0 +1,14 @@
+package com.asl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegistrationWithEmailVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegistrationWithEmailVerificationApplication.class, args);
+		System.out.println("Login Registration With Email Verification Application.......");
+	}
+
+}
